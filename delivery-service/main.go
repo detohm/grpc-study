2f8f97e91bfeea4801f9b371a621947feee8db75
Attaphong Rattanaveerachanon <ohm.attaphong@gmail.com>
2021-11-27 15:25:17 +0700

    Experiment with grpc on both server and client code - add delivery service as a server code - add order service as a client code - add dependencies in go module

--- delivery-service/main.go
+++ delivery-service/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
 
+	pb "github.com/detohm/grpc-study/proto"
+	"google.golang.org/grpc"
+)
+
+type DeliveryServer struct {
+	pb.UnimplementedDeliverySVCServer
+}
+
+func NewDeliveryServer() *DeliveryServer {
+	return &DeliveryServer{}
+}
+
+func (d *DeliveryServer) Create(ctx context.Context, p *pb.Product) (*pb.Delivery, error) {
+	log.Printf("create called with product : %v", p)
+	return &pb.Delivery{
+		DeliveryId: 1,
+		TrackingId: "d3u2dfn2lh2e2dss",
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:5000")
+	if err != nil {
+		fmt.Println("")
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterDeliverySVCServer(grpcServer, NewDeliveryServer())
+	grpcServer.Serve(lis)
 }
--- order-service/main.go
+++ order-service/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/detohm/grpc-study/proto"
+	"google.golang.org/grpc"
+)
+
+func runCreate(client pb.DeliverySVCClient, product *pb.Product) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	delivery, err := client.Create(ctx, product)
+	if err != nil {
+		log.Printf("error occurred: %v", err)
+	}
+	log.Printf("got delivery: %v", delivery)
+
+}
+
 func main() {
 
+	opts := []grpc.DialOption{}
+	opts = append(opts, grpc.WithInsecure())
+
+	conn, err := grpc.Dial("localhost:5000", opts...)
+	if err != nil {
+		log.Printf("error occurred: %v", err)
+	}
+	defer conn.Close()
+
+	product := pb.Product{
+		ProductId: 1,
+		UserId:    1,
+	}
+
+	client := pb.NewDeliverySVCClient(conn)
+
+	runCreate(client, &product)
 }
